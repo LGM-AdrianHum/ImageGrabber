c3a2a4224105a9b58d0060dac21dc85be3d31bc7
Andrew Gray <andrew.paul.gray@gmail.com>
2015-11-28 23:15:22 +1100

    shorter

--- ImageGrabber/Program.cs
+++ ImageGrabber/Program.cs
@@ -2,29 +2,25 @@
 using System.Net;
 using IG.Core;
 
-namespace ImageGrabber
-{
-    class Program
-    {
+namespace ImageGrabber {
+    internal class Program {
         private static void Main(string[] args) {
             var options = new Options(args);
 
             using (var webClient = new WebClient()) {
-                for (var i = 1; i < 100000000; i++) {
+                for (var i = 1; i < 5000; i++) {
                     var filename = i + ".jpg";
                     var sourceFile = options.TargetUrlBase + filename;
                     var outFile = options.OutDirectory + filename;
                     try {
                         webClient.DownloadFile(new Uri(sourceFile), outFile);
-                        Console.WriteLine("Downloaded {0} to {1}.", sourceFile, outFile);
+                        Console.WriteLine($"Downloaded {sourceFile} to {outFile}.");
                     }
                     catch (WebException w) {
-                        Console.WriteLine("{0} does not exist.", sourceFile);
+                        Console.WriteLine($"{sourceFile} does not exist.");
                     }
                 }
             }
-
-
         }
     }
 }
